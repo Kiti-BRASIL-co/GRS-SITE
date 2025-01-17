962b1f11e5e74e2fdb548db218b3d762023fa026
MsDoors.gg <msdoors.gg@gmail.com>
2025-01-17 12:31:47 -0300

    new Library

--- Scripts/Msdoors/executavel.lua
+++ Scripts/Msdoors/executavel.lua
@@ -44,7 +44,7 @@ local SUPPORTED_GAMES = {
     [110258689672367] = "Doors/OldLobby.lua",
     [189707] = "NaturalDisaster/places/game.lua",
     [12137249458] = "CampoFFA/ArmasFFA.lua",
-    [5275822877] = "Carrinho%2Bcart-para-Giganoob/CartGiganoob.lua",
+    [5275822877] = "Carrinho%2Bcart-para-Giganoob/game.lua",
     [9285238704] = "Raceclicker/game.lua"
 }
 
